35e0bb95dcfd3b46f9199678edeb3ed9f2f51204
Cuong Phung <cuong.phung@muaban.net>
2019-12-24 16:11:52 +0700

    add react

---
+++ react101/precache-manifest.4a7bc34cf20b0a7b0afa5b4acf95f2cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5326d709e1dc1551023b8b4c1ec1b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2ca37e7ea4ee54b20e6",
+    "url": "/static/css/main.7870187f.chunk.css"
+  },
+  {
+    "revision": "ced5b0e390a06fc2d7ea",
+    "url": "/static/js/2.c1794475.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.c1794475.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2ca37e7ea4ee54b20e6",
+    "url": "/static/js/main.a1154b54.chunk.js"
+  },
+  {
+    "revision": "7f798bcb15504130021b",
+    "url": "/static/js/runtime-main.cf9573be.js"
+  }
+]);
