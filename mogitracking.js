c507b6daf43f814f9f1184e0c52a0e7a30a62202
Cuong Phung <chicuongphung@gmail.com>
2020-04-22 16:38:35 +0700

    add...

--- mogitracking.js
+++ mogitracking.js
@@ -1,3 +1,8 @@
+OneSignal.push(["getNotificationPermission", function(permission) {
+  console.log("Site Notification Permission:", permission);
+  // (Output) Site Notification Permission: default
+}]);
+
 var MogiTracking = window.MogiTracking || {};
 
 var MogiTracking = (function() {
