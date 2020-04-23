39e8ea61ce4d3553e277d8cb446fd3259ab5e000
Cuong Phung <chicuongphung@gmail.com>
2020-04-23 10:18:56 +0700

    add...

--- mogitracking.js
+++ mogitracking.js
@@ -1,8 +1,20 @@
 var MogiTracking = window.MogiTracking || {};
 
 var MogiTracking = (function () {
-  return {
 
+  const AddTracking = function (player_id) {
+    const payload = {
+      id: player_id
+    };
+
+    fetch('/Tracking/Addtracking', {
+      method: 'POST', 
+      body: JSON.stringify(payload)
+    });
+
+  };
+  return {
+    AddTracking:AddTracking
   };
 
 
