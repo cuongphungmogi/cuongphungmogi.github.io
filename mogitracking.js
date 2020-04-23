3c858b507bca9b8096473a1f88fcc500bf8bfcad
Cuong Phung <chicuongphung@gmail.com>
2020-04-23 09:33:27 +0700

    add...

--- mogitracking.js
+++ mogitracking.js
@@ -1,22 +1,27 @@
 OneSignal.push(function () {
-  /* These examples are all valid */
-  OneSignal.isPushNotificationsEnabled(function (isEnabled) {
-    if (isEnabled)
-    {
-      const ext_id = MOGI.ProfileId;
-      // OneSignal.setExternalUserId(ext_id);
-    };
-  });
 
+  OneSignal.isPushNotificationsEnabled(function (isEnabled) {
+    if (isEnabled) {
+      const ext_id = MOGI.ProfileId;
+      OneSignal.setExternalUserId(ext_id);
+    };
+
+  });
+  OneSignal.on('subscriptionChange', function (isSubscribed) {
+    if (isSubscribed) {
+      const ext_id = MOGI.ProfileId;
+      OneSignal.setExternalUserId(ext_id);
+    }
+  });
 });
 
 
 var MogiTracking = window.MogiTracking || {};
 
-var MogiTracking = (function() {
-return {
+var MogiTracking = (function () {
+  return {
 
-};
+  };
 
 
 })();
